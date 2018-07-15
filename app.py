bb675232dae470cb9952b84709d1b21a09fe653c
waaron78 <41174163+waaron78@users.noreply.github.com>
2018-07-15 12:23:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 @app.route('/<name>')
 def profile(name):
 	new_name = str(name) + " likes to eat mangos"
-	return render_template('index.html', name=name_name)
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
